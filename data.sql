e64448cde6fb6309d613f44e9c5f4444894886f0
Wahyu Azizi <wahyuazizi03@gmail.com>
2025-10-08 13:24:17 +0800

    sql update

---
+++ data.sql
@@ -0,0 +1,24 @@
+Create table 
+karyawan (
+	id serial primary key,
+	nama varchar(100) not null,
+	email varchar(50) not null,
+	tanggal_bergabung date not null default current_date
+);
+create table absensi (
+	id serial primary key,
+	karyawan_id int,
+	tanggal date not null default current_date,
+	status_hadir boolean not null default false,
+	constraint fk_karyawan foreign key(karyawan_id) references karyawan(id) on delete set null,
+	constraint fk_status check(status_hadir)
+);
+
+INSERT INTO karyawan(nama, email) VALUES
+('zoro', 'zoro@email.com'),
+('sanji', 'sanji@email.com');
+
+insert into absensi(karyawan_id) values
+(1),
+(2),
+(3);
